7b9a1e63e9fb448b7c3ef8db09585a7d5986039e
Ketan <68579341+Ketan-Verma@users.noreply.github.com>
2022-11-24 19:41:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,20 +6,14 @@ header.addEventListener("click",function(e){
    e.target.classList.add("active");
 });
 
-let imgs = ["./images/img1.JPG","./images/img2.JPG","./images/img3.png","./images/img4.png"]
-let hs = ["MES","Boom!","Good","another"]
-let ps = ["A software that is designed to optimise manufacturing processes...","hello this is MES","catch phrase","catch phrase"]
+let imgs = ["./images/img1.JPG","./images/img2.JPG","./images/img3.JPG","./images/img4.JPG"]
 let img = document.getElementById("img-home");
-let h = document.getElementById("glass-h")
-let p = document.getElementById("glass-p")
 img.src = imgs[0];
 
 setInterval(tictic, 4000);
 let i = 1 ;
 function tictic() {
 img.src = imgs[i];
-h.innerText = hs[i]
-p.innerText = ps[i]
 i++;
 if(i>=imgs.length)i=0;
 
