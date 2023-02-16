7e888ed8bb84ec8470571ac16858195a699543bf
Ketan Verma <ketancation@gmail.com>
2023-02-16 14:09:34 +0530

    done changes

--- app.js
+++ app.js
@@ -6,18 +6,18 @@ header.addEventListener("click",function(e){
    e.target.classList.add("active");
 });
 
-let imgs = ["./images/img1.JPG","./images/img2.JPG","./images/img3.JPG","./images/img4.JPG"]
-let img = document.getElementById("img-home");
-img.src = imgs[0];
+// let imgs = ["./images/img1.JPG","./images/img2.JPG","./images/img3.JPG","./images/img4.JPG"]
+// let img = document.getElementById("img-home");
+// img.src = imgs[0];
 
-setInterval(tictic, 4000);
-let i = 1 ;
-function tictic() {
-img.src = imgs[i];
-i++;
-if(i>=imgs.length)i=0;
+// setInterval(tictic, 4000);
+// let i = 1 ;
+// function tictic() {
+// img.src = imgs[i];
+// i++;
+// if(i>=imgs.length)i=0;
 
-}
+// }
 
 function toogle(){
  let nav = document.getElementById("nav-bar");
